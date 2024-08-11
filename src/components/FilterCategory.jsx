88eecab7ec085f324d243a3ad70e0a2e4d94c4d0
Sanjay Rai <sanjayharper9852@gmail.com>
2024-08-11 20:59:49 +0530

    Applied the filter logic to display filter products

--- src/components/FilterCategory.jsx
+++ src/components/FilterCategory.jsx
@@ -1,10 +1,6 @@
 import { Checkbox, CheckboxGroup, Row } from 'rsuite';
 
-const FilterCategory = () => {
-  const handleFilter = value => {
-    console.log(`The filter value is ${value}`);
-  };
-
+const FilterCategory = ({ handleFilter, displayedProduct, totalProduct }) => {
   return (
     <div>
       FilterCategory
@@ -13,7 +9,18 @@ const FilterCategory = () => {
           <Checkbox value="inStock" onChange={handleFilter}>
             In Stock
           </Checkbox>
+          <Checkbox value="delivery" onChange={handleFilter}>
+            Can Deliver
+          </Checkbox>
+          <Checkbox value="expensive" onChange={handleFilter}>
+            Expensive ($100+)
+          </Checkbox>
         </CheckboxGroup>
+      </Row>
+      <Row>
+        <div>
+          Showing {displayedProduct} out of {totalProduct}
+        </div>
       </Row>
     </div>
   );
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,18 +5,47 @@ import ProductCard from '../components/ProductCard';
 import { displayMessage } from '../misc/helper';
 import { useCart, useUpdateCart } from '../misc/cart.context';
 import FilterCategory from '../components/FilterCategory';
+import { useState } from 'react';
 
 const Category = () => {
   const { categoryId } = useParams();
-
-  const filterCategory = products.filter(
-    data => data.categoryId === categoryId
-  );
 
   const cart = useCart();
   const setCart = useUpdateCart();
 
   const toaster = useToaster();
+
+  const [originalFilterProducts] = useState(() => {
+    const productData = products.filter(data => data.categoryId === categoryId);
+
+    return productData ? productData : [];
+  });
+
+  const [filterValue, setFilterValue] = useState({
+    inStock: false,
+    delivery: false,
+    expensive: false,
+  });
+
+  const applyFilter = () => {
+    let result = originalFilterProducts;
+
+    if (filterValue.inStock) {
+      result = result.filter(data => data.inStock === true);
+    }
+
+    if (filterValue.delivery) {
+      result = result.filter(data => data.delivery === true);
+    }
+
+    if (filterValue.expensive) {
+      result = result.filter(data => data.price >= 100);
+    }
+
+    return result;
+  };
+
+  const filterCategory = applyFilter();
 
   const handleAddToCard = (id, price, name) => {
     const isInCart = cart.find(item => item.id === id);
@@ -39,12 +68,71 @@ const Category = () => {
     });
   };
 
+  const handleFilter = (value, isChecked) => {
+    switch (value) {
+      case 'inStock':
+        isChecked
+          ? setFilterValue(val => {
+              return {
+                ...val,
+                inStock: (val.inStock = true),
+              };
+            })
+          : setFilterValue(val => {
+              return {
+                ...val,
+                inStock: (val.inStock = false),
+              };
+            });
+        break;
+
+      case 'delivery':
+        isChecked
+          ? setFilterValue(val => {
+              return {
+                ...val,
+                delivery: (val.delivery = true),
+              };
+            })
+          : setFilterValue(val => {
+              return {
+                ...val,
+                delivery: (val.delivery = false),
+              };
+            });
+        break;
+
+      case 'expensive':
+        isChecked
+          ? setFilterValue(val => {
+              return {
+                ...val,
+                expensive: (val.expensive = true),
+              };
+            })
+          : setFilterValue(val => {
+              return {
+                ...val,
+                expensive: (val.expensive = false),
+              };
+            });
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
       <Container>
         <Row>
           <Col sm={24} md={12} lg={12}>
-            <FilterCategory />
+            <FilterCategory
+              handleFilter={handleFilter}
+              displayedProduct={filterCategory.length}
+              totalProduct={originalFilterProducts.length}
+            />
           </Col>
           <Col as="ul" sm={24} md={12} lg={12}>
             <h2>Category {categoryId}</h2>
